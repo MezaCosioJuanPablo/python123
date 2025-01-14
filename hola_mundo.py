fa4a668d0318e1ff96588d841f9bc13ee05f069b
MezaCosioJuanPablo <mezacosiojuanpablo@gmail.com>
2025-01-14 14:59:25 -0600

    py

---
+++ hola_mundo.py
@@ -0,0 +1,2 @@
+#impresion de hola mundo en python 
+print("impresión de hola mundo en python")
---
+++ impresion_variable.py
@@ -0,0 +1,18 @@
+# Impresión de variables
+
+nombre = "kevin"
+print(nombre)
+
+numero_uno = 10
+numero_dos = 5
+
+# impresión de sumas 
+
+print(numero_uno+numero_dos)
+
+# Impresión de concatenaciones 
+
+nombre_completo = "Kevin Armando"
+apellidos_completos="Luciano García"
+
+print(nombre_completo + " " + apellidos_completos) 
