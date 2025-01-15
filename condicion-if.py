8b7e7c5d5ec294d197f41b1aca5482b2415136b5
MezaCosioJuanPablo <mezacosiojuanpablo@gmail.com>
2025-01-15 11:46:03 -0600

    condiciones

---
+++ condicion-if.py
@@ -0,0 +1,11 @@
+#En este documento usaremos la condición IF
+#La condición IF se usa para tomar decisiones 
+
+#Declaramos unas variables 
+edad = 7
+
+#Realizamos una evaluacíon de la condición 
+if edad < 18: 
+        print("Eres menor de edad")
+else: 
+        print("Eres mayor de edad")
---
+++ operaciones.py
@@ -0,0 +1,17 @@
+#Definición de dos variables en python para hacer calculos matematicos 
+
+#Definición de variables
+a = 10
+b = 5
+
+#Operaciones matematicas 
+suma = a + b
+resta = a - b 
+multiplicacion = a * b
+division = a / b 
+
+#Impresión de resultados 
+print("La suma de a y b es: ", suma)
+print("La resta de a y b es: ", resta)
+print("La multiplicacion de a y b es: ", multiplicacion)
+print("La división de a y b es: ", division)
